34052001dd1ffd7bd145a4a0ea790cdb2776c9c6
Diman Todorov <diman.todorov@outlook.com>
2020-02-09 19:32:27 -0800

    some more interesting functions

--- HW3/DataBuilder.cpp
+++ HW3/DataBuilder.cpp
@@ -4,8 +4,29 @@
 
 std::shared_ptr<Model> DataBuilder::createData(GLuint program, UniformGrid & grid)
 {
-	auto scienceFunction = [](float x, float y) { return exp(-(x * x + y * y)); };
+	auto scienceFunction = [](float x, float y) {
+		float d = 0.8;
+		float x1 = x + d;
+		float x2 = x - d;
+		float y1 = y + d;
+		float y2 = y - d;
+		return exp(-(x1 * x1 + y1 * y1)) + exp(-(x2 * x2 + y2 * y2));
+	};
 	//auto scienceFunction = [](float x, float y) { return sin(1 / (x * x + y * y)); };
+
+	// fractional brownian motion
+	//auto scienceFunction = [](float x, float y) {
+	//	float amplitude = 1.;
+	//	float frequency = 1.;
+	//	float z = sin(x * frequency);
+	//	float t = 0.01*(-y * 130.0);
+	//	z += sin(x*frequency*2.1 + t)*4.5;
+	//	z += sin(x*frequency*1.72 + t * 1.121)*4.0;
+	//	z += sin(x*frequency*2.221 + t * 0.437)*5.0;
+	//	z += sin(x*frequency*3.1122 + t * 4.269)*2.5;
+	//	z *= amplitude * 0.06;
+	//	return z;
+	//};
 
 	// assuming that v.z = f(x, y)
 	auto normalFunction = [](glm::vec3 v) {
@@ -14,6 +35,10 @@ std::shared_ptr<Model> DataBuilder::createData(GLuint program, UniformGrid & gri
 			-2 * v.y * v.z,
 			1.0f);
 	};
+
+	//auto normalFunction = [](glm::vec3 v) {
+	//	return glm::vec3(0.0f, 1.0f, 0.0f);
+	//};
 
 	grid.sample(scienceFunction);
 
